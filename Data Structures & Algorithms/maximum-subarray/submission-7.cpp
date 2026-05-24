93f7d9e2ebd19f46ab988caf001a58885cd031a3
ProBluebbes <84875256+ProBluebbes@users.noreply.github.com>
2026-05-23 19:18:17 -0700

    Add: maximum-subarray - submission-7

---
+++ Data Structures & Algorithms/maximum-subarray/submission-7.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+
+        int start = 0;
+        int end = 0;
+        int sum = nums[0];
+        int cumulativeSum = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            cumulativeSum += nums[i];
+
+            // CHECK IF RESETING
+            if (nums[i] > cumulativeSum) {
+                start = i;
+                end = i;
+                cumulativeSum = nums[i];
+                sum = max(cumulativeSum, sum);
+            }
+            // CHECK IF ADDING IS WORTHWHILE
+            else if (cumulativeSum >= sum) {
+                end = i;
+                sum = cumulativeSum;
+            }
+        }
+
+        return sum;
+    }
+};
